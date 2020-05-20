e198ef90acd2274fa3485d879987498f11fa82bb
Dave <dryan76@gmail.com>
2020-05-20 14:15:41 -0400

    Added functionality to add tags from instances.

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -5,14 +5,28 @@ session = boto3.Session(profile_name='shotty')
 ec2 = session.resource('ec2')
 
 @click.command()
-def list_instances():
-    for i in ec2.instances.all():
+@click.option('--project', default=None,
+    help="Only instances for project (tag Project:<name>)")
+def list_instances(project):
+    "List EC2 instances"
+    instances = []
+
+    if project:
+        filters = [{'Name':'tag:Project', 'Values':[project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+
+    for i in instances:
+        tags = { t['Key']: t['Value'] for t in i.tags or [] }
         print(', '.join((
             i.id,
             i.instance_type,
             i.placement['AvailabilityZone'],
             i.state['Name'],
-            i.public_dns_name)))
+            i.public_dns_name,
+            tags.get('Project', '<no project>')
+            )))
 
     return
         
